fd89fb0d7549d1ee394099b9ef2f2f24c5fc2c47
Kaylee Ervin <kaylee.ervin1510@gmail.com>
2026-07-08 14:50:42 -0700

    will work on perfecting the code

--- test_OOP.py
+++ test_OOP.py
@@ -1,3 +1,5 @@
 from OOP_practice_code import Animatronic
 
-glam_freddy = ("Freddy", 1111111111, "Glamrock", "Bear", "Rockstar Row", )
+glam_freddy = ("Freddy", 1111111111, "Glamrock", "Bear", "Rockstar Row", 95)
+
+print(glam_freddy)
